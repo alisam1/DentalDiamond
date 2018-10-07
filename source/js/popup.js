23e5976067ed6776b75f4fd32bf007ec9ee28ced
alisam1 <alisa-m2@yandex.ru>
2018-10-07 21:20:26 +0300

    double modal

--- source/js/popup.js
+++ source/js/popup.js
@@ -6,6 +6,34 @@ $(document).ready(function() { // Ждём загрузки страницы
 
 });
 
+$(document).ready(function() { // Ждём загрузки страницы
+
+	$(".close").click(function(){	// Событие клика на затемненный фон
+		$(".popup-alarm").fadeOut(800);	// Медленно убираем всплывающее окно
+	});
+
+});
+
 function showPopup() {
 	$(".popup").fadeIn(800); // Медленно выводим изображение
 }
+
+$(document).ready(function() { // Ждём загрузки страницы
+
+	$(".close").click(function(){	// Событие клика на затемненный фон
+		$(".popup").fadeOut(800);	// Медленно убираем всплывающее окно
+	});
+
+});
+
+function showPopupAlarm() {
+	$(".popup-alarm").fadeIn(800); // Медленно выводим изображение
+}
+
+$(document).ready(function() { // Ждём загрузки страницы
+
+	$(".close").click(function(){	// Событие клика на затемненный фон
+		$(".popup-alarm").fadeOut(800);	// Медленно убираем всплывающее окно
+	});
+
+});
