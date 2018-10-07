844dc0d52274f54572894a8d0f8affd312c084ab
alisam1 <alisa-m2@yandex.ru>
2018-10-07 19:01:04 +0300

    modal

--- build/js/popup.js
+++ build/js/popup.js
@@ -11,19 +11,4 @@ $(function () {
 		$('#overlay').remove('#overlay');
 		return false;
 	});
-
-	//script for tabs
-	$("div.selectTabs").each(function () {
-		var tmp = $(this);
-		$(tmp).find(".lineTabs li").each(function (i) {
-			$(tmp).find(".lineTabs li:eq("+i+") a").click(function(){
-				var tab_id=i+1;
-				$(tmp).find(".lineTabs li").removeClass("active");
-				$(this).parent().addClass("active");
-				$(tmp).find(".tab_content div").stop(false,false).hide();
-				$(tmp).find(".tab"+tab_id).stop(false,false).fadeIn(300);
-				return false;
-			});
-		});
-	});
 });
