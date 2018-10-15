0c6347123a81d135084ff414105e299aecab536c
alisam1 <alisa-m2@yandex.ru>
2018-10-16 02:51:17 +0300

    kartochka

--- source/js/popup.js
+++ source/js/popup.js
@@ -37,3 +37,71 @@ $(document).ready(function() { // Ждём загрузки страницы
 	});
 
 });
+
+(function($) {
+  $(function() {
+      var popwindow = $('.popup');
+      var popbutton = $('.appointment');
+      $("body").prepend("<div class='mask'></div>");
+      function preparewindow(windowobject) {
+        var winwidth = windowobject.data("width");
+        var winheight = windowobject.data("height");
+        var winmargin = winwidth / 2;
+        var wintitle = windowobject.data("title");
+
+        windowobject.wrap("<div class='popup'></div>");
+        windowobject.addClass("box_window_in");
+        windowobject.parent(".box_window").prepend("<div class='bw_close'>аЗаАаКбббб</div>");
+        windowobject.css("cursor","pointer");
+      }
+      if (popwindow.length) {
+        preparewindow(popwindow);
+        popbutton.click(function(){
+            var idwind = $(this).data("window");
+            $("#" + idwind).parent(".popup").fadeIn().addClass("windactiv");
+            $(".mask").fadeIn();
+            $(".to_blur").addClass("blur");
+        });
+      };
+      $(".mask, .close").click(function(){
+          $(".windactiv").fadeOut();
+          $(".windactiv").removeClass("windactiv");
+          $(".mask").fadeOut();
+           $(".to_blur").removeClass("blur");
+      });
+  });
+})(jQuery);
+
+(function($) {
+  $(function() {
+      var popwindow = $('.popup-alarm');
+      var popbutton = $('.alarm');
+      $("body").prepend("<div class='mask'></div>");
+      function preparewindow(windowobject) {
+        var winwidth = windowobject.data("width");
+        var winheight = windowobject.data("height");
+        var winmargin = winwidth / 2;
+        var wintitle = windowobject.data("title");
+
+        windowobject.wrap("<div class='popup'></div>");
+        windowobject.addClass("box_window_in");
+        windowobject.parent(".box_window").prepend("<div class='close'></div>");
+        windowobject.css("cursor","pointer");
+      }
+      if (popwindow.length) {
+        preparewindow(popwindow);
+        popbutton.click(function(){
+            var idwind = $(this).data("window");
+            $("#" + idwind).parent(".popup").fadeIn().addClass("windactiv");
+            $(".mask").fadeIn();
+            $(".to_blur").addClass("blur");
+        });
+      };
+      $(".mask, .close").click(function(){
+          $(".windactiv").fadeOut();
+          $(".windactiv").removeClass("windactiv");
+          $(".mask").fadeOut();
+           $(".to_blur").removeClass("blur");
+      });
+  });
+})(jQuery);
