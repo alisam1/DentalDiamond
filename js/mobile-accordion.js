c985132982178fc73c5d3cccc0bead7fc5afbf03
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-25 16:39:01 +0000

    Deploy alisam1/DentalDiamond to github.com/alisam1/DentalDiamond.git:gh-pages

--- js/mobile-accordion.js
+++ js/mobile-accordion.js
@@ -2,11 +2,11 @@ $(function() {
 
 $(".menu_item--clinika a").click(function() {
 var ul = $(this).next(),
-    clone = ul.clone().css({"height":"auto"}).appendTo("body"),
-    height = ul.css("height") === "0px" ? ul[0].scrollHeight + "px" : "0px";
+    clone = $(".accordion_list").clone().css({"height":"auto"}).appendTo("body"),
+    height = $(".accordion_list").css("height") === "0px" ? ul[0].scrollHeight + "px" : "0px";
 
 clone.remove();
-ul.animate({"height":height});
+$(".accordion_list").animate({"height":height});
 return false;
 });
 });
